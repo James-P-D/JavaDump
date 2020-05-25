4cff619c11b2fd4d13419357f77bc36e4a83fe7b
James Dorrington <jdorrington@hotmail.com>
2020-05-25 12:11:12 +0100

    Adding stuff for displaying Unicode in Java on Windows Console

---
+++ src/OutputSpecialCharacters/src/OutputSpecialCharacters.java
@@ -0,0 +1,9 @@
+import java.io.UnsupportedEncodingException;
+
+public class OutputSpecialCharacters {
+
+	public static void main(String[] args) {
+    	//Λλᴧ
+		Console.println("\u039B\u03BB\u1D27");
+	}
+}
---
+++ src/OutputSpecialCharacters/src/doit.bat
@@ -0,0 +1,2 @@
+javac -cp ..\libs\jna-5.5.0.jar;..\libs\jna-platform-5.5.0.jar;. OutputSpecialCharacters.java
+java -classpath ..\libs\jna-5.5.0.jar;..\libs\jna-platform-5.5.0.jar;. OutputSpecialCharacters
